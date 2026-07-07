67f43fa0a290149718c215462442f6bd5dfc895d
quintas902 <manuquintas89@gmail.com>
2026-07-07 13:57:37 +0200

    chore(auth): configure rich user model

---
+++ app/Models/User.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+final class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /*
+     * @var array<int, string>
+     */
+    protected $fillable = ["name", "email", "password"];
+
+    /*
+     * @var array<string, string>
+     */
+    protected $hidden = ["password", "remember_token"];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            "email_verified_at" => "datetime",
+            "password" => "hashed",
+        ];
+    }
+}
